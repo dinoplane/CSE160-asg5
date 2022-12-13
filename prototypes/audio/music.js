449ecc85f32226b7989eb5e650e773e5fd1ca762
dinoplane <isacsac2017@gmail.com>
2022-12-12 18:07:43 -0800

    init

--- prototypes/audio/music.js
+++ prototypes/audio/music.js
@@ -1,1 +1,18 @@
-jjhh
+export class MusicPlayer{
+    constructor(){
+        this.context = new AudioContext();
+        this.channels = [];
+    }
+
+    addChannel(type="sine"){
+        let c = this.context.createOscillator();
+        c.type = type;
+        
+        c.connect(this.context.destination);
+        c.start();
+    }
+
+    setChannelStartGain(){
+        
+    }
+}
---
+++ prototypes/audio/test.js
@@ -0,0 +1,20 @@
+import {MusicPlayer} from "./music.js"
+
+
+
+function addActionsToHTMLUI(){
+    document.getElementById("p1").onclick() = function(ev){
+
+    };
+
+    document.getElementById("stop").onclick() = function(ev){
+
+    };
+}
+
+function main(){
+
+    addActionsToHTMLUI();
+}
+
+main();
